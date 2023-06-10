b501979d2a9cdc1d312fd9005f28062d86d63047
Nahid-07 <nahidasif1998@gmail.com>
2023-06-11 00:10:56 +0600

    env added

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
+require("dotenv").config()
+
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middleware
 
@@ -12,6 +15,31 @@ app.use(express.json());
 app.get('/', async(req, res)=>{
     res.send("server is running")
 });
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ugpmzsn.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+    
+  } finally {
+    
+    
+  }
+}
+run().catch(console.dir);
+
 
 app.listen(port, ()=>{
     console.log("app listening");

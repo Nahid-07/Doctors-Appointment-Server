7c6edebf82ada3884ca932983b0117bd74219e88
Nahid-07 <nahidasif1998@gmail.com>
2023-06-14 00:45:34 +0600

    service booked item removes from availaible options

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-require("dotenv").config()
+require("dotenv").config();
 
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // middleware
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', async(req, res)=>{
-    res.send("server is running")
+app.get("/", async (req, res) => {
+  res.send("server is running");
 });
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ugpmzsn.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -26,38 +24,59 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    const appointmentOptions = client.db("Doctors-Appointment").collection("appointmentOptions");
+    const appointmentOptions = client
+      .db("Doctors-Appointment")
+      .collection("appointmentOptions");
     // booking collenctions
-    const bookingCollections = client.db("Doctors-Appointment").collection("bookingCollections");
+    const bookingCollections = client
+      .db("Doctors-Appointment")
+      .collection("bookingCollections");
 
-    app.get('/appointmentOptions', async(req,res)=>{
-        const querry = {};
-        const options = await appointmentOptions.find(querry).toArray();
-        res.send(options)
-    })
+    app.get("/appointmentOptions", async (req, res) => {
+      // console.log(date);
+      const querry = {};
+      const options = await appointmentOptions.find(querry).toArray();
+
+      const date = req.query.date;
+      const bookingQuery = { treatmentDate: date };
+      const alreadyBooked = await bookingCollections
+        .find(bookingQuery)
+        .toArray();
+
+      options.forEach((option) => {
+        const serviceBooked = alreadyBooked.filter(
+          (booked) => booked.treatmentName === option.name
+        );
+        const serviceBookedSlot = serviceBooked.map(
+          (bookedService) => bookedService.slot
+        );
+        const remainingSlots = option.slots.filter(
+          (slot) => !serviceBookedSlot.includes(slot)
+        );
+        option.slots = remainingSlots;
+      });
+
+      // console.log(alreadyBooked);
+      res.send(options);
+    });
 
     // post booking data api
 
-    app.post('/bookings', async(req, res)=>{
+    app.post("/bookings", async (req, res) => {
       const bookings = req.body;
       const result = await bookingCollections.insertOne(bookings);
-      res.send(result)
-
-    })
-    
+      res.send(result);
+    });
   } finally {
-    
-    
   }
 }
 run().catch(console.log());
 
-
-app.listen(port, ()=>{
-    console.log("app listening");
-})
+app.listen(port, () => {
+  console.log("app listening");
+});

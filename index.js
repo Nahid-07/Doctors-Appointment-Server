aa21ee51ba54f5a76108b5f85af28bd379017314
Nahid-07 <nahidasif1998@gmail.com>
2023-08-31 13:20:13 +0600

    admin role get api created

--- index.js
+++ index.js
@@ -112,9 +112,18 @@ async function run() {
       res.send(allUsers)
     });
 
+    // admin api get
+
+    app.get("/users/admin/:email", async(req, res)=>{
+      const email = req.params.email;
+      const query = {email : email};
+      const user = await usersCollections.findOne(query);
+      res.send({isAdmin : user?.role})
+    })
+
     // admin role api
 
-    app.put('/users/:id', async(req, res)=>{
+    app.put('/users/admin/:id', async(req, res)=>{
       const id = req.params.id;
       const email = req.body.email;
       const query = {email : email};
